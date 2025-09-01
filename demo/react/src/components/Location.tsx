d92c88a223ddfcad69d5c79cb397e0b528d06292
ofins <jkyeewang@gmail.com>
2025-09-01 09:47:24 +0800

    fix: display location info

--- demo/react/src/components/Location.tsx
+++ demo/react/src/components/Location.tsx
@@ -1,31 +1,5 @@
 import { useLocation } from "@ofins/client/react";
 
-// getLocation
-// refresh
-// clearCache
-// startWatching
-// stopWatching
-/*
-function LocationExample() {
-  const {
-    location,
-    loading,
-    error,
-    isFromCache,
-    permissionState,
-    getLocation,
-    refresh,
-    clearCache,
-  } = useLocation({
-    autoFetch: true,
-    enableHighAccuracy: true,
-    maxCacheDuration: 300_000, // 5 minutes
-    onSuccess: (loc) => console.log('Got location!', loc),
-    onError: (err) => console.error('Location error:', err),
-    debug: true,
-  });
-*/
-
 const Location = () => {
   const {
     location,
@@ -52,7 +26,22 @@ const Location = () => {
     isFromCache,
     permissionState,
   });
-  return <div>Location</div>;
+  return (
+    <div>
+      <h2>Location</h2>
+      {loading && <p>Loading...</p>}
+      {error && <p>Error: {error.message}</p>}
+      {location && (
+        <ul>
+          <li>Latitude: {location.latitude}</li>
+          <li>Longitude: {location.longitude}</li>
+          <li>Accuracy: {location.accuracy} meters</li>
+          <li>From Cache: {isFromCache ? "Yes" : "No"}</li>
+          <li>Permission State: {permissionState}</li>
+        </ul>
+      )}
+    </div>
+  );
 };
 
 export default Location;
